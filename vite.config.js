bf25ecd9c52285e7f628b966d4ab438837498b21
santilaraa <santilarae@gmail.com>
2026-03-13 05:12:20 +0000

    publish v2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite'
 // Reemplaza 'humanidades-digitales-economia-naranja' por el nombre exacto 
 // que le darás a tu repositorio en GitHub
 export default defineConfig({
-    base: '/humanidades-digitales-economia-naranja/'
+    base: '/hd-full-economia-naranja/'
 })
